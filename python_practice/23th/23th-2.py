182a9969615a85e9854a30192a005b2aa29806ec
Celox16 <sck04184@naver.com>
2021-08-03 17:47:12 +0900

    feat: edit 23th directory
    more practice python

---
+++ python_practice/23th/23th-2.py
@@ -0,0 +1,14 @@
+from openpyxl import load_workbook
+wb = load_workbook('simple_data.xlsx')
+data = wb.active
+
+row = data['2']
+print(row)
+for cell in row:
+    print(cell.value)
+
+print('-'*20)
+
+col = data['A']
+for cell in col:
+    print(cell.value)
