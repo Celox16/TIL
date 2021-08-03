b4db8df4d7c6bef5b85c2b481e85201639e40f33
Celox16 <sck04184@naver.com>
2021-08-03 13:47:24 +0900

    feat: add python practice
    to practice python practice directory

---
+++ python_practice/8th-2.py
@@ -0,0 +1,10 @@
+a = ['alghost', 'fastcampus']
+
+if 'alghost' in a:
+    print('Alghost exits')
+
+if('algho') in a:
+    print('algho exits')
+
+if 'alghost' in a and 'fastcampus' in a:
+    print('eh')
---
+++ python_practice/second.py
@@ -0,0 +1,5 @@
+dict_val = {'name':'taewha', 'company': 'fastcampus'}
+
+for (key,value) in dict_val.items():
+    print(key)
+    print(value)   
